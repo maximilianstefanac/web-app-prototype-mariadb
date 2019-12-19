20ae914e1ba419c90999bb290149307652c3f804
maximilianstefanac <maximilian.stefanac@googlemail.com>
2019-12-19 21:43:02 +0100

    added missing semicolons and added srid usage comment for PointToText Functions

--- spatial-database.sql
+++ spatial-database.sql
@@ -13,21 +13,21 @@ INSERT INTO webgis.tree VALUES
     (3, NULL, PointFromText('POINT(8.677184 49.218310)')),
     (4, NULL, PointFromText('POINT(8.677112 49.217826)')),
     (5, NULL, PointFromText('POINT(8.677103 49.217069)')),
-    (6, NULL, PointFromText('POINT(8.676717 49.216577)'))
+    (6, NULL, PointFromText('POINT(8.676717 49.216577)'));
 
 CREATE TABLE webgis.plot (
     plot_id INT PRIMARY KEY,
     plot_name VARCHAR(100),
     boundary POLYGON NOT NULL 
-)
+);
 
 -- Last Point in Polygon must be the first one - other than in leaflet
 INSERT INTO webgis.plot VALUES
-    (1, 'Obst-Gen-Garten', PolygonFromText('POLYGON((8.677058 49.218531,8.676849 49.216258,8.674748 49.216280,8.674712 49.216217,8.676846 49.216199,8.676861 49.216100,8.677283 49.216188,8.677462 49.218530, 8.677058 49.218531))'))
+    (1, 'Obst-Gen-Garten', PolygonFromText('POLYGON((8.677058 49.218531,8.676849 49.216258,8.674748 49.216280,8.674712 49.216217,8.676846 49.216199,8.676861 49.216100,8.677283 49.216188,8.677462 49.218530, 8.677058 49.218531))'));
 
 -- select tree which position is in the polygon of the Obst-Gen-Garten -> trees with id 1 - 5
-SELECT * FROM webgis.tree as t WHERE within(t.`position`, (SELECT boundary FROM webgis.plot WHERE plot_id = 1 LIMIT 1))
+SELECT * FROM webgis.tree as t WHERE within(t.`position`, (SELECT boundary FROM webgis.plot WHERE plot_id = 1 LIMIT 1));
 
 
 -- transform geo data-type to GeoJSON in SELECT Statement
-SELECT ST_AsGeoJSON(position, 6) FROM webgis.tree
+SELECT ST_AsGeoJSON(position, 6) FROM webgis.tree;
